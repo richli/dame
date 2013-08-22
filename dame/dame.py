28fdf1934e8b34c2cc67dbdd531923a5231827f9
Richard Lindsley <rich@dranek.com>
2013-08-22 16:51:35 -0600

    Re-create main window programmatically now
    Also, I switch from PySide back to PyQt4

--- dame/dame.py
+++ dame/dame.py
@@ -2,83 +2,23 @@ __author__ = "Richard Lindsley"
 
 import sys, os
 
-#import sip
-#sip.setapi('QDate', 2)
-#sip.setapi('QDateTime', 2)
-#sip.setapi('QString', 2)
-#sip.setapi('QTextStream', 2)
-#sip.setapi('QTime', 2)
-#sip.setapi('QUrl', 2)
-#sip.setapi('QVariant', 2)
-#from PyQt4 import Qt
+import sip
+sip.setapi('QDate', 2)
+sip.setapi('QDateTime', 2)
+sip.setapi('QString', 2)
+sip.setapi('QTextStream', 2)
+sip.setapi('QTime', 2)
+sip.setapi('QUrl', 2)
+sip.setapi('QVariant', 2)
+from PyQt4 import QtCore,QtGui
 
-from PySide.QtCore import *
-from PySide.QtGui import *
-from main_window import Ui_MainWindow
-
+from .ui.mainwindow import MainWindow
 from loadsir import loadsir
 
-class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
-        self.setupUi(self)
-
-        # Menu handlers
-        self.actionAbout.triggered.connect(self.menu_about)
-        self.actionOpen.triggered.connect(self.menu_open)
-        self.actionClose.triggered.connect(self.menu_close)
-
-        # Add icons to menu items (can't seem to do it in Qt Designer)
-        # http://stackoverflow.com/questions/11643221/are-there-default-icons-in-pyqt-pyside
-        QIcon.setThemeName("gnome") # TODO: temporary
-        self.actionQuit.setIcon(QIcon.fromTheme("application-exit"))
-        self.actionOpen.setIcon(QIcon.fromTheme("document-open"))
-        self.actionAbout.setIcon(QIcon.fromTheme("help-about"))
-        self.actionClose.setIcon(QIcon.fromTheme("window-close"))
-
-        # Status bar
-        self.statusbar.showMessage("No file loaded")
-
-    def menu_about(self):
-        """ Display about popup """
-        QMessageBox.about(self, "About", 
-                "Dame\nCopyright 2013 Richard Lindsley\nDame is a SIR file viewer")
-        
-    def menu_open(self):
-        """ Display open file dialog """
-        filename, file_filter = QFileDialog.getOpenFileName(self, 
-                "Open SIR file", 
-                os.getenv("HOME", os.getcwd()), 
-                "SIR files (*.sir *.ave);;Any file (*)"
-                )
-        if filename != '':
-            self.load_sir(filename)
-
-    def menu_close(self):
-        """ Close file """
-        print("close clicked")
-
-    def load_sir(self, fname):
-        """ Loads the SIR file and updates display """
-        self.sirdata = loadsir(fname)
-        self.update_image()
-
-    def update_image(self):
-        """ Reload the image """
-        nsx = int(self.sirdata[1][0].astype('int'))
-        nsy = int(self.sirdata[1][1].astype('int'))
-        image = QImage(nsx, nsy, QImage.Format_ARGB32)
-        for x in xrange(nsx):
-            for y in xrange(nsy):
-                pix_val = qRgba(1, 2, 3, 255)
-                image.setPixel(x, y, pix_val)
-        pixmap = QPixmap.fromImage(image)
-        #self.main_im.pixmap = pixmap
-        self.main_im.setText("Loaded")
-        # TODO: Finish here
+#class Dame(QtGui.QApplication):
 
 def main():
-    qt_app = QApplication(sys.argv)
+    qt_app = QtGui.QApplication(sys.argv)
     frame = MainWindow()
     frame.show()
     qt_app.exec_()
---
+++ dame/ui/mainwindow.py
@@ -0,0 +1,115 @@
+import os
+from textwrap import dedent
+from PyQt4 import QtCore, QtGui
+from dame import version_string
+
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self, parent)
+        self.setupUi()
+
+    def setupUi(self):
+        self.setWindowTitle("dame")
+        # TODO: Set window icon
+        self.create_actions()
+        self.create_statusbar()
+        self.create_menus()
+
+        self.imageLabel = QtGui.QLabel()
+        #self.imageLabel.setBackgroundRole(QtGui.QPalette.Base)
+        self.imageLabel.setSizePolicy(QtGui.QSizePolicy.Ignored,
+                QtGui.QSizePolicy.Ignored)
+        self.imageLabel.setScaledContents(True)
+
+        self.scrollArea = QtGui.QScrollArea()
+        self.scrollArea.setBackgroundRole(QtGui.QPalette.Dark)
+        self.scrollArea.setWidget(self.imageLabel)
+
+        self.setCentralWidget(self.scrollArea)
+
+    def create_statusbar(self):
+        self.statusBar().showMessage("Ready")
+        self.pixinfo_label = QtGui.QLabel()
+        self.pixinfo_label.setVisible(False)
+
+        self.statusBar().addPermanentWidget(self.pixinfo_label)
+
+    def create_actions(self):
+        # TODO: Add icons
+
+        self.about_action = QtGui.QAction("&About", self)
+        self.about_action.setStatusTip("About dame")
+        self.about_action.setMenuRole(QtGui.QAction.AboutRole)
+        self.about_action.triggered.connect(self.show_about)
+
+        self.open_action = QtGui.QAction("&Open", self)
+        self.open_action.setStatusTip("Open a SIR file")
+        self.open_action.setShortcut(QtGui.QKeySequence.Open)
+        self.open_action.triggered.connect(self.open_file)
+
+        self.close_action = QtGui.QAction("&Close", self)
+        self.close_action.setStatusTip("Close current SIR file")
+        self.close_action.setShortcut(QtGui.QKeySequence.Close)
+        self.close_action.triggered.connect(self.close_file)
+
+        self.exit_action = QtGui.QAction("E&xit", self)
+        self.about_action.setMenuRole(QtGui.QAction.QuitRole)
+        self.exit_action.setStatusTip("Exit dame")
+        self.exit_action.setShortcut(QtGui.QKeySequence("Ctrl+Q"))
+        self.exit_action.triggered.connect(self.close)
+
+        # http://stackoverflow.com/questions/11643221/are-there-default-icons-in-pyqt-pyside
+        QtGui.QIcon.setThemeName("gnome") # TODO: temporary
+        self.open_action.setIcon(QtGui.QIcon.fromTheme("document-open"))
+        self.close_action.setIcon(QtGui.QIcon.fromTheme("window-close"))
+        self.exit_action.setIcon(QtGui.QIcon.fromTheme("application-exit"))
+        self.about_action.setIcon(QtGui.QIcon.fromTheme("help-about"))
+
+    def create_menus(self):
+        menu = self.menuBar().addMenu("&File")
+        menu.addAction(self.open_action)
+        menu.addAction(self.close_action)
+        menu.addSeparator()
+        menu.addAction(self.exit_action)
+        menu = self.menuBar().addMenu("&Help")
+        menu.addAction(self.about_action)
+
+    def open_file(self):
+        """ Display open file dialog """
+        filename, file_filter = QtGui.QFileDialog.getOpenFileName(self, 
+                "Open SIR file", 
+                QtCore.QDir.homePath(), 
+                "SIR files (*.sir *.ave);;Any file (*)"
+                )
+        if filename:
+            print("Loading {}".format(filename))
+            self.sirdata = loadsir(filename)
+            # TODO
+            #self.update_image()
+
+    def close_file(self):
+        """ Close file """
+        print("Close file clicked")
+
+    def show_about(self):
+        """ Display about popup """
+        about_text= """
+                Dame {}
+                Copyright 2013 Richard Lindsley
+                Dame is a SIR file viewer""".format(version_string)
+        QtGui.QMessageBox.about(self, "About", dedent(about_text))
+
+    #def update_image(self):
+    #    """ Reload the image """
+    #    nsx = int(self.sirdata[1][0].astype('int'))
+    #    nsy = int(self.sirdata[1][1].astype('int'))
+    #    image = QImage(nsx, nsy, QImage.Format_ARGB32)
+    #    for x in xrange(nsx):
+    #        for y in xrange(nsy):
+    #            pix_val = qRgba(1, 2, 3, 255)
+    #            image.setPixel(x, y, pix_val)
+    #    pixmap = QPixmap.fromImage(image)
+        #self.main_im.pixmap = pixmap
+        #self.main_im.setText("Loaded")
+        # TODO: Finish here
+    
