1c1f2cab677ead5f3cf3aa59c5094a741378e5bc
Richard Lindsley <rich@dranek.com>
2013-08-22 00:01:57 -0600

    Use pyside instead of pyqt4

--- dame/dame.py
+++ dame/dame.py
@@ -2,19 +2,23 @@ __author__ = "Richard Lindsley"
 
 import sys
 
-import sip
-sip.setapi('QDate', 2)
-sip.setapi('QDateTime', 2)
-sip.setapi('QString', 2)
-sip.setapi('QTextStream', 2)
-sip.setapi('QTime', 2)
-sip.setapi('QUrl', 2)
-sip.setapi('QVariant', 2)
-from PyQt4 import Qt
+#import sip
+#sip.setapi('QDate', 2)
+#sip.setapi('QDateTime', 2)
+#sip.setapi('QString', 2)
+#sip.setapi('QTextStream', 2)
+#sip.setapi('QTime', 2)
+#sip.setapi('QUrl', 2)
+#sip.setapi('QVariant', 2)
+#from PyQt4 import Qt
+
+from PySide.QtCore import *
+from PySide.QtGui import *
+
 
 def main():
-    qt_app = Qt.QApplication(sys.argv)
-    label = Qt.QLabel("Hello, world")
+    qt_app = QApplication(sys.argv)
+    label = QLabel("Hello, world")
     label.show()
     qt_app.exec_()
 
